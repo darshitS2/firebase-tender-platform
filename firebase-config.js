983c9449aa76cf74cf1e6d40c647b2260a1dc6a5
darshitS2 <darshits0102@gmail.com>
2025-06-13 17:16:31 +0530

    Delete firebase-config.js

--- firebase-config.js
+++
@@ -1,8 +0,0 @@
-const firebaseConfig = {
-            apiKey: "AIzaSyDB58R9pORY3wZ6Qme1--FRckwdj9aD_Ok",
-            authDomain: "tender-platform-6e348.firebaseapp.com",
-            projectId: "tender-platform-6e348",
-            storageBucket: "tender-platform-6e348.firebasestorage.app",
-            messagingSenderId: "437023509723",
-            appId: "1:437023509723:web:bbac4980cb945679c410fe"
-        };
